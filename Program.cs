a4b0cd2acf2db7a30aa777a0bdd301b5fabcdb61
Slavec46 <41571219+Slavec46@users.noreply.github.com>
2020-05-13 21:20:30 +0400

    Simple little program for test branching

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstTryTasker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z = 1.09;
+            double y = 9.91;
+            double x = z + y;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(x);
+        }
+    }
+}
